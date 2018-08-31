f22db8c04aed3a88c959b5c7c32fd466bbdc29ae
adamlstephenson <astephensonx@gmail.com>
2018-08-30 23:34:03 -0500

    sidebar html finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,11 +31,39 @@ class Navbar extends Component {
                         <li>
                             <a href="#">
                                 <i className="fas fa-briefcase"></i>
+                                About
                             </a>
+                            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">
+                                <i className="fas fa-copy"></i>
+                                Pages
+                            </a>
+                            <ul className="collapse list-unstyled" id="pageSubmenu">
+                                <li>
+                                    <a href="#">Page 1</a>
+                                </li>
+                                <li>
+                                    <a href="#">Page 2</a>
+                                </li>
+                                <li>
+                                    <a href="#">Page 3</a>
+                                </li>
+                            </ul>
                         </li>
                     </ul>
 
                 </nav>
+
+                {/* Page Content */}
+                <div id="content">
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                        <div className="container-fluid">
+                            <button type="button" id="sidebarCollapse" className="btn btn-info">
+                                <i className="fas fa-align-left"></i>
+                                <span>Toggle Sidebar</span>
+                            </button>
+                        </div>
+                    </nav>
+                </div>
             </div>
         )
     }
